70e10eb6e1fb336daa724160b2638d27049d3420
Lilie de la Motte <liliedelamotte@gmail.com>
2018-09-30 12:34:54 -0700

    adds empty methods

--- src/ArrayList.java
+++ src/ArrayList.java
@@ -1,4 +1,105 @@
+// ldelamotte17@georgefox.edu
 
 public class ArrayList {
 
+    /** 
+     * 
+     */
+    public ArrayList() {
+        
+    }
+
+
+    /**
+     * 
+     * @param initialCapacity
+     */
+    public ArrayList(int initialCapacity) {
+        
+    }
+
+
+    /**
+     * 
+     * @param index
+     * @param element
+     */
+    public void add (int index, E element) {
+        
+    }
+
+
+    /**
+     * 
+     */
+    public boolean add (E element) {
+        
+    }
+
+
+    /**
+     * 
+     */
+    public void clear() {
+        
+    }
+
+
+    /**
+     * 
+     * @param index
+     * @return
+     */
+    public E get(int index) {
+        
+    }
+
+
+    /**
+     * 
+     * @param element
+     * @return
+     */
+    public int indexOf(E element) {
+        
+    }
+
+
+    /**
+     * 
+     * @return
+     */
+    public boolean isEmpty() {
+        
+    }
+
+
+    /**
+     * 
+     * @param index
+     * @return
+     */
+    public E remove (int index) {
+        
+    }
+
+
+    /**
+     * 
+     * @param index
+     * @param element
+     * @return
+     */
+    public E set(int index, E element) {
+        
+    }
+
+
+    /**
+     * 
+     * @return
+     */
+    public int size() {
+        
+    }
 }
